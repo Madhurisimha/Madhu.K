4ea76cbba05f430262110ec11869b000a0eb8c97
Madhurisimha <145712118+Madhurisimha@users.noreply.github.com>
2023-09-23 13:13:43 +0000

    operations

---
+++ basics/operation.js
@@ -0,0 +1,27 @@
+let value = 3
+let negValue = -value 
+ // console.log(negValue); -3
+
+ //console.log(2+2);
+ //console.log(2**2);
+
+ let str1 = 1
+ let str2 = 2
+ let str3 = str1+str2
+ // console.log(str3); // 3 
+
+ // console.log("1"+2);//12 
+ // console.log( 2+2+"1"); //41 
+
+ // console.log(true); true 
+// console.log(+true);  1 => incrementing 
+
+
+
+
+
+
+
+
+
+
